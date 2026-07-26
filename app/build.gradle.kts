d1c7e38892cb673e866df442ee9ec21bbbe7883d
prabin <prabin.tims@gmail.com>
2026-07-26 09:00:53 -0700

    Bump releaseCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ configure<ApplicationExtension> {
         applicationId = "prabin.timsina.unlockhabit"
         minSdk = 30
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
